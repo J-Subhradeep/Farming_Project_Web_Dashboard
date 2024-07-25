c50ff2445466557332f4fbc1cf4526a30477f2a8
krishnendu roy <krishnendu.kalyani1@gmail.com>
2024-07-25 21:32:02 +0530

    Login and dashboard fixed

--- intelligent-agricultural-management-platform/src/components/home/Home.jsx
+++ intelligent-agricultural-management-platform/src/components/home/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
-import Styles from "./Home.module.css";
+import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
+import Dashboard from "../Dashboard/Dashboard";
+import Leftsidebar from "../leftsidebar/Leftsidebar";
 
 const Home = () => {
   const navigator = useNavigate();
@@ -13,10 +15,21 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-      <Navbar />
-    </>
+    <Contianer>
+      <Leftsidebar />
+      <Dashboard />
+    </Contianer>
   );
 };
 
 export default Home;
+
+const Contianer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  height: 100vh;
+  background-color: #f5f6fa;
+  overflow: hidden;
+`;
--- intelligent-agricultural-management-platform/src/components/leftsidebar/Leftsidebar.jsx
+++ intelligent-agricultural-management-platform/src/components/leftsidebar/Leftsidebar.jsx
@@ -1,7 +1,119 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-const Leftsidebar = () => {
-  return <div>Leftsidebar</div>;
+import { IoHomeOutline } from "react-icons/io5";
+import { RxComponent1 } from "react-icons/rx";
+
+// Sidebar container
+const SidebarContainer = styled.div`
+  width: 250px;
+  background-color: #fff;
+  padding: 20px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 10px 0px 20px -7px rgba(189, 189, 189, 1);
+`;
+
+// Individual sections
+const Section = styled.div`
+  margin-bottom: 20px;
+`;
+
+// Section title
+const SectionTitle = styled.h3`
+  font-size: 16px;
+  color: #333;
+  margin-bottom: 10px;
+`;
+
+// Links within sections
+const SectionLink = styled.a`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  color: #555;
+  text-decoration: none;
+  margin: 5px 0;
+  gap: 5px;
+
+  &:hover {
+    color: #007bff;
+  }
+`;
+
+const Devider = styled.hr`
+  border: 0;
+  height: 1px;
+  background: #dcdcdc;
+  margin: 20px 0;
+`;
+
+const LeftSidebar = () => {
+  const HandleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  };
+
+  return (
+    <SidebarContainer>
+      <SectionTitle>Navigation</SectionTitle>
+      <Devider />
+      <SectionLink href="#">
+        <IoHomeOutline /> Dashboard
+      </SectionLink>
+      <Devider />
+      <SectionLink href="#">
+        <RxComponent1 />
+        Component
+      </SectionLink>
+      <Devider />
+      <SectionLink href="#">
+        <RxComponent1 />
+        Component
+      </SectionLink>
+      <Devider />
+      <SectionLink href="#">
+        <RxComponent1 />
+        Component
+      </SectionLink>
+      <Devider />
+      <SectionLink href="#">
+        <RxComponent1 />
+        Component
+      </SectionLink>
+      {/* <Section>
+        <SectionTitle>Navigation</SectionTitle>
+        <SectionLink href="#">
+          <SectionLink href="#">
+            <IoHomeOutline /> Dashboard
+          </SectionLink>
+        </SectionLink>
+      </Section>
+      <Devider />
+      <Section>
+        <SectionTitle>Utilities</SectionTitle>
+        <SectionLink href="#">
+          <RxComponent1 />
+          Component
+        </SectionLink>
+      </Section>
+      <Devider />
+      <Section>
+        <SectionTitle>Support</SectionTitle>
+        <SectionLink href="#">Sample Page</SectionLink>
+        <Devider />
+        <SectionLink href="#">Documentation</SectionLink>
+      </Section>
+      <Devider />
+       */}
+      <Devider />
+      <Section>
+        <SectionTitle>Authentication</SectionTitle>
+        <SectionLink onClick={HandleLogout}>Logout</SectionLink>
+      </Section>
+    </SidebarContainer>
+  );
 };
 
-export default Leftsidebar;
+export default LeftSidebar;
